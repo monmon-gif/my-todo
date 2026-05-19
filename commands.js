017a39843fe415a0705c5fb6c7240c3297c0ec09
S306669-002\s306669 <takagawa>
2026-05-19 16:06:09 +0900

    module構成の変更

--- commands.js
+++ commands.js
@@ -3,20 +3,11 @@ const dayjs  = require('dayjs');
 
 const chalk = require('chalk');
 const uuid  = require('uuid');
-const axios = require('axios');
 
-const fileHandling = require('./fileManager');
-const { saveTaskList, getTaskList } = fileHandling;
-const taskRepository = require('./taskRepository');
-const { findTaskById, clearTask, partialMatchList, getCompletedTasks } = taskRepository;
+const kintoneClient = require('./kintoneClient');
+const { taskRegister, taskList, taskIdSearch, taskDone, taskDelete, taskPartialMatch } = kintoneClient;
 const taskFormatter = require('./taskFormatter');
 const { formatTask } = taskFormatter;
-
-const dotenv = require('dotenv');
-dotenv.config();
-const KINTONE_BASE_URL = process.env.KINTONE_BASE_URL;
-const KINTONE_APP_ID = process.env.KINTONE_APP_ID;
-const KINTONE_API_TOKEN = process.env.KINTONE_API_TOKEN;
 
 // タスクを登録
 async function register(title, priority) {
@@ -29,213 +20,107 @@ async function register(title, priority) {
   }
   const taskId = uuid.v4();
   const createdAt = dayjs().format(`YYYY-MM-DD HH:mm`);
+  // タスク内容
+  const taskContent = { taskId, title, priority, createdAt };
 
-  try {
-    await axios.post(`${KINTONE_BASE_URL}/k/v1/record.json`, {
-      app: KINTONE_APP_ID,
-      record: {
-        taskId: { value: taskId },
-        title: { value: title },
-        done: { value: [] },
-        priority: { value: priority },
-        createdAt: { value: createdAt }
-      }
-    }, {
-      headers: {
-        'X-Cybozu-API-Token': KINTONE_API_TOKEN,
-        'Content-Type': 'application/json'
-      }
-    });
-    console.log(chalk.default.green(`タスクを追加しました。`));
-  } catch (error) {
-    console.error(chalk.default.red(`タスクの追加に失敗しました。`));
-    console.error(error.status || error.response.data);
+  const isRegistered = await taskRegister(taskContent);
+  if (isRegistered) {
+    console.log(chalk.default.green(`タスクを登録しました。`));
+  } else {
+    console.log(chalk.default.red(`タスクの登録に失敗しました。`));
   }
 };
 
 // タスクの一覧表示
 async function list(options) {
-  try {
-    const response = await axios.get(`${KINTONE_BASE_URL}/k/v1/records.json`, {
-      headers: {
-        'X-Cybozu-API-Token': KINTONE_API_TOKEN,
-      },
-      params: {
-        app: KINTONE_APP_ID,
-        query: options.done ? `done in ("完了")` : options.todo ? `done not in ("完了")` : undefined
-      }
-    });
-    if (response.data.records.length === 0) {
-      console.log(`タスクがありません。`);
+  const response = await taskList(options);
+  if(response.length === 0){
+    if (options.done) {
+      console.log((`完了したタスクがありません。`));
+      return;
+    } else if (options.todo) {
+      console.log((`未完了のタスクがありません。`));
       return;
     }
-    const tasks = response.data.records;
-    if(tasks.length === 0){
-      if (options.done) {
-        console.log((`完了したタスクがありません。`));
-        return;
-      } else if (options.todo) {
-        console.log((`未完了のタスクがありません。`));
-        return;
-      }
-      console.log(`タスクがありません。`);
-      return;
-    }
-    formatTask(tasks);
-  } catch (error) {
-    console.error(chalk.default.red(`タスクの取得に失敗しました。`));
-    console.error(error.status || error.response.data);
+    console.log(`タスクがありません。`);
     return;
   }
+  formatTask(response);
 }
 
 // タスクを完了
 async function done(taskId) {
-  try {
-    const response = await axios.get(`${KINTONE_BASE_URL}/k/v1/records.json`, {
-      headers: {
-        'X-Cybozu-API-Token': KINTONE_API_TOKEN,
-      },
-      params: {
-        app: KINTONE_APP_ID,
-        query: `taskId = "${taskId}"`
-      }
-    });
-
-    if (response.data.records.length === 0) {
-      console.log(`タスクIDが見つかりませんでした。`);
-      return;
-    }
-
-    const recordId = response.data.records[0].$id.value;
-
-    await axios.put(`${KINTONE_BASE_URL}/k/v1/record.json`, {
-      app: KINTONE_APP_ID,
-      id: recordId,
-      record: {
-        done: { value: ["完了"] }
-      }
-    }, {
-      headers: {
-        'X-Cybozu-API-Token': KINTONE_API_TOKEN,
-        'Content-Type': 'application/json'
-      }
-    });
-    console.log(chalk.default.green(`タスクを完了しました。`));
-  } catch (error) {
-    console.error(chalk.default.red(`タスクの完了に失敗しました。`));
-    console.error(error.status || error.response.data);
+  const response = await taskIdSearch(taskId);
+  if (response.length === 0) {
+    console.log(`タスクIDが見つかりませんでした。`);
     return;
+  }
+  const recordId = response[0].$id.value;
+
+  const isDone = await taskDone(recordId);
+  if (isDone) {
+    console.log(chalk.default.green(`タスクを完了しました。`));
+  } else {
+    console.log(chalk.default.red(`タスクの完了に失敗しました。`));
   }
 }
 
 // タスクを削除
 async function deleteTask(taskId) {
-  try {
-    const response = await axios.get(`${KINTONE_BASE_URL}/k/v1/records.json`, {
-      headers: {
-        'X-Cybozu-API-Token': KINTONE_API_TOKEN,
-      },
-      params: {
-        app: KINTONE_APP_ID,
-        query: `taskId = "${taskId}"`
-      }
-    });
-
-    if (response.data.records.length === 0) {
-      console.log(`タスクIDが見つかりませんでした。`);
-      return;
-    }
-    const recordId = response.data.records[0].$id.value;
-    console.log(recordId);
-    await axios.delete(`${KINTONE_BASE_URL}/k/v1/records.json`, {
-      headers: {
-        'X-Cybozu-API-Token': KINTONE_API_TOKEN,
-      },
-      data: {
-        app: KINTONE_APP_ID,
-        ids: [recordId]
-      }
-    });
-    console.log(chalk.default.yellow(`タスクを削除しました。`));
-  } catch (error) {    
-    console.error(chalk.default.red(`タスクの削除に失敗しました。`));
-    console.error(error.status || error.response.data);
+  const response = await taskIdSearch(taskId);
+  if (response.length === 0) {
+    console.log(`タスクIDが見つかりませんでした。`);
     return;
+  }
+  const recordId = response[0].$id.value;
+
+  const isDeleted = await taskDelete(recordId);
+  if (isDeleted) {
+    console.log(chalk.default.yellow(`タスクを削除しました。`));
+  } else {
+    console.log(chalk.default.red(`タスクの削除に失敗しました。`));
   }
 }
 
 // タスク名の部分一致検索
 async function partialMatch(title) {
-  try {
-    const response = await axios.get(`${KINTONE_BASE_URL}/k/v1/records.json`, {
-      headers: {
-        'X-Cybozu-API-Token': KINTONE_API_TOKEN,
-      },
-      params: {
-        app: KINTONE_APP_ID,
-        query: `title like "${title}"`
-      }
-    });
-    if (response.data.records.length === 0) {
-      console.log(`一致するタスクがありません。`);
-      return;
-    }
-    const tasks = response.data.records;
-    formatTask(tasks);
-  } catch (error) {
-    console.error(chalk.default.red(`タスクの検索に失敗しました。`));
-    console.error(error.status || error.response.data);
+  const response = await taskPartialMatch(title);
+  if (response.length === 0) {
+    console.log(`一致するタスクがありません。`);
     return;
   }
+  formatTask(response);
 }
 
 async function statisticsDisplay() {
 
-  try {
-    // タスクの取得
-    const response = await axios.get(`${KINTONE_BASE_URL}/k/v1/records.json`, {
-      headers: {
-        'X-Cybozu-API-Token': KINTONE_API_TOKEN,
-      },
-      params: {
-        app: KINTONE_APP_ID
-      }
-    });
-    if (response.data.records.length === 0) {
-      console.log(`タスクがありません。`);
-      return;
-    }
-    // 全タスク数
-    const tasks = response.data.records.length;
-    // 直近7日以内に作成されたタスク数
-    const oneWeekAgo = dayjs().subtract(7, 'day');
-    const oneWeekTasks = response.data.records.filter(task => {
-      return !dayjs(task.createdAt.value).isBefore(oneWeekAgo);
-    });
-    // 完了タスク数
-    const completedTasks = response.data.records.filter(task => {
-      return task.done.value.includes("完了");
-    }).length;
-    // 完了率
-    const completionRate = Math.round((completedTasks / tasks) * 100);
+  // タスクの取得
+  const response = await taskList({});
+  // 全タスク数
+  const tasks = response.length;
+  // 直近7日以内に作成されたタスク数
+  const oneWeekAgo = dayjs().subtract(7, 'day');
+  const oneWeekTasks = response.filter(task => {
+    return !dayjs(task.createdAt.value).isBefore(oneWeekAgo);
+  });
+  // 完了タスク数
+  const completedTasks = response.filter(task => {
+    return task.done.value.includes("完了");
+  }).length;
+  // 完了率
+  const completionRate = Math.round((completedTasks / tasks) * 100);
 
-    if(tasks === 0){
-      console.log(`タスクがありません。`);
-      return;
-    } else if (oneWeekTasks.length === 0) {
-      console.log(`直近7日以内に作成したタスクがありません。`);
-    } else {
-      console.log(`全タスク数: ${tasks}`);
-      console.log(`完了タスク数: ${completedTasks}`);
-      console.log(`未完了タスク数: ${tasks - completedTasks}`);
-      console.log(`完了率: ${completionRate}%`);
-      console.log(`直近7日以内に作成されたタスク数: ${oneWeekTasks.length}`);
-    }
-  } catch (error) {   
-    console.error(chalk.default.red(`タスクの取得に失敗しました。`));
-    console.error(error.status || error.response.data);
+  if(tasks === 0){
+    console.log(`タスクがありません。`);
     return;
+  } else if (oneWeekTasks.length === 0) {
+    console.log(`直近7日以内に作成したタスクがありません。`);
+  } else {
+    console.log(`全タスク数: ${tasks}`);
+    console.log(`完了タスク数: ${completedTasks}`);
+    console.log(`未完了タスク数: ${tasks - completedTasks}`);
+    console.log(`完了率: ${completionRate}%`);
+    console.log(`直近7日以内に作成されたタスク数: ${oneWeekTasks.length}`);
   }
 }
 
---
+++ kintoneClient.js
@@ -0,0 +1,141 @@
+const axios = require('axios');
+
+const dotenv = require('dotenv');
+dotenv.config();
+const KINTONE_BASE_URL = process.env.KINTONE_BASE_URL;
+const KINTONE_APP_ID = process.env.KINTONE_APP_ID;
+const KINTONE_API_TOKEN = process.env.KINTONE_API_TOKEN;
+
+// タスク登録
+async function taskRegister(taskContent){
+  console.log(taskContent);
+  try {
+    await axios.post(`${KINTONE_BASE_URL}/k/v1/record.json`, {
+    app: KINTONE_APP_ID,
+    record: {
+      taskId: { value: taskContent.taskId },
+      title: { value: taskContent.title },
+      done: { value: [] },
+      priority: { value: taskContent.priority },
+      createdAt: { value: taskContent.createdAt }
+    }
+  }, {
+    headers: {
+      'X-Cybozu-API-Token': KINTONE_API_TOKEN,
+      'Content-Type': 'application/json'
+    }
+  });
+  return true;
+  } catch (error) {
+    console.error(error.status);
+    console.error(error.response.data);
+    return false;
+  }
+}
+
+async function taskList(options) {
+  try {
+    const response = await axios.get(`${KINTONE_BASE_URL}/k/v1/records.json`, {
+      headers: {
+        'X-Cybozu-API-Token': KINTONE_API_TOKEN,
+      },
+      params: {
+        app: KINTONE_APP_ID,
+        query: options.done ? `done in ("完了")` : options.todo ? `done not in ("完了")` : undefined
+      }
+    });
+    return response.data.records;
+  } catch (error) {
+    console.error(error.status);
+    console.error(error.response.data);
+    return [];
+  }
+}
+
+async function taskIdSearch(taskId) {
+  try {
+    const response = await axios.get(`${KINTONE_BASE_URL}/k/v1/records.json`, {
+      headers: {
+        'X-Cybozu-API-Token': KINTONE_API_TOKEN,
+      },
+      params: {
+        app: KINTONE_APP_ID,
+        query: `taskId = "${taskId}"`
+      }
+    });
+    return response.data.records;
+  } catch (error) {
+    console.error(error.status);
+    console.error(error.response.data);
+    return [];
+  }
+}
+
+async function taskDone(recordId) {
+  try {
+    await axios.put(`${KINTONE_BASE_URL}/k/v1/record.json`, {
+      app: KINTONE_APP_ID,
+      id: recordId,
+      record: {
+        done: { value: ["完了"] }
+      }
+    }, {
+      headers: {
+        'X-Cybozu-API-Token': KINTONE_API_TOKEN,
+        'Content-Type': 'application/json'
+      }
+    });
+    return true;
+  } catch (error) {
+    console.error(error.status);
+    console.error(error.response.data);
+    return false;
+  }
+}
+
+async function taskDelete(recordId) {
+  try {
+    await axios.delete(`${KINTONE_BASE_URL}/k/v1/records.json`, {
+      headers: {
+        'X-Cybozu-API-Token': KINTONE_API_TOKEN,
+      },
+      data: {
+        app: KINTONE_APP_ID,
+        ids: [recordId]
+      }
+    });
+    return true;
+  } catch (error) {
+    console.error(error.status);
+    console.error(error.response.data);
+    return false;
+  }
+}
+
+async function taskPartialMatch(title) {
+  try {
+    const response = await axios.get(`${KINTONE_BASE_URL}/k/v1/records.json`, {
+      headers: {
+        'X-Cybozu-API-Token': KINTONE_API_TOKEN,
+      },
+      params: {
+        app: KINTONE_APP_ID,
+        query: `title like "${title}"`
+      }
+    });
+    return response.data.records;
+  } catch (error) {
+    console.error(error.status);
+    console.error(error.response.data);
+    return [];
+  }
+}
+
+module.exports = {
+  taskRegister,
+  taskList,
+  taskIdSearch,
+  taskDone,
+  taskDelete,
+  taskPartialMatch
+};
