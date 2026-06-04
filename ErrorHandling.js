9c8b3e3857374ca548907b62731cff3f10beb01c
S306669-002\s306669 <takagawa>
2026-06-04 14:38:19 +0900

    エラーレスポンス400固定の修正 #44

--- ErrorHandling.js
+++ ErrorHandling.js
@@ -3,17 +3,18 @@ const chalk = require('chalk');
 // エラーハンドリング
 function responseError(error){
   if (error.response !== undefined) {
-    console.log(chalk.default.red(error.response.status));
     if (error.response.status === 400) {
-      console.error(chalk.default.red(`APIトークンが間違っています。`));
+      console.error(chalk.default.red(`APIトークンが正しくありません`));
+    } else if (error.response.status === 403) {
+      console.error(chalk.default.red(`アプリIDが正しくありません。`));
+    } else {
+      console.error(chalk.default.red(error.response.data.message));
     }
   } else {
-    console.log(chalk.default.red(error.code));
     if (error.code === 'ENOTFOUND' || error.code === 'ECONNREFUSED') {
       console.error(chalk.default.red(`ネットワークエラー（kintoneに接続できません。）`));
     }
   }
-  console.error(chalk.default.red(error.message));
 }
 
 module.exports = {
